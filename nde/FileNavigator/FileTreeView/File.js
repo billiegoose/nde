f1279b82d465f9b9ba363f4067a04d5e9ad21d7b
Will Hilton <wmhilton@gmail.com>
2017-08-02 08:54:01 -0400

    Use 'default-icon' fallback for files without a special icon

--- nde/FileNavigator/FileTreeView/File.js
+++ nde/FileNavigator/FileTreeView/File.js
@@ -2,7 +2,7 @@ import 'file-icons-js/css/style.css'
 import icons from 'file-icons-js'
 
 export default function File ({filename, ...props}) {
-  let className = 'icon ' + icons.getClassWithColor(filename);
+  let className = 'icon ' + (icons.getClassWithColor(filename) || 'default-icon');
   let {filepath, root, statedata, FolderComponent, FileComponent, glEventHub, glContainer, ...props2} = props
   return (
     <label {...props2}>
