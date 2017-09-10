f323e88e8339ea3f2f30145abd482d57fc0de28b
Will Hilton <wmhilton@gmail.com>
2017-09-10 19:24:38 -0400

    Working git again + Delete File menu option

--- nde/FileNavigator/FileNavigatorFileComponent.js
+++ nde/FileNavigator/FileNavigatorFileComponent.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import File from './FileTreeView/File.js'
+import FileIcon from './FileTreeView/FileIcon.js'
 import StatusIcon from './FileTreeView/StatusIcon.js'
+import { ContextMenu, SubMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu/dist/react-contextmenu.js";
+import fs from 'fs'
+import cuid from 'cuid'
 
 export default class FileNavigatorFileComponent extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      cuid: cuid(),
+    }
+  }
   componentDidMount () {
     this.props.glContainer.layoutManager.createDragSource(ReactDOM.findDOMNode(this), {
       type:'react-component',
@@ -17,12 +27,23 @@ export default class FileNavigatorFileComponent extends React.Component {
       props: { filepath: this.props.filepath }
     })
   }
+  deleteFile () {
+    fs.unlink(this.props.filepath)
+  }
   render () {
     let {disableContextMenu, filename} = this.props
     return (
-      <File filename={filename} domProps={{onDoubleClick: this.doubleclick.bind(this)}}>
-        &nbsp;<StatusIcon status='untracked' />
-      </File>
+      <ContextMenuTrigger id={this.state.cuid} disable={disableContextMenu}>
+        <File filename={filename} domProps={{onDoubleClick: this.doubleclick.bind(this)}}>
+          &nbsp;<StatusIcon status='untracked' />
+        </File>
+
+        <ContextMenu id={this.state.cuid}>
+          <MenuItem onClick={() => this.deleteFile()}>
+            Delete File <i className="icon trash-icon"></i>
+          </MenuItem>
+        </ContextMenu>
+      </ContextMenuTrigger>
     )
   }
 }
