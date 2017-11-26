e5a5a22511fac6cb1d1501a8045f23f9971ac668
William Hilton <wmhilton@gmail.com>
2017-11-26 21:46:13 +0000

    Fix things the latest release of SweetAlert2 broke

--- components/SweetAlert.js
+++ components/SweetAlert.js
@@ -5,12 +5,14 @@ export async function prompt (text) {
   if (typeof text === 'string') {
     return swal({
       text,
-      input: 'text'
+      input: 'text',
+      useRejections: true
     })
   } else if (typeof text === 'object') {
     if (text.input === undefined) {
       text.input = 'text'
     }
+    text.useRejections = true
     return swal(text)
   }
 }
